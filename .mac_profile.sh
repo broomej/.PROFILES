09666289e9e69dce11994ecdfbc288d9b5d552a7
Jai Broome <jbroome@uw.edu>
2017-04-23 11:38:47 -0700

    added mac profiles

---
+++ .mac_profile.sh
@@ -0,0 +1,8 @@
+# Preferences specific to macOS machines
+
+# Aliases
+alias f='open -a Finder ./'                 # f:            Opens current directory in MacOS Finder
+trash () { command mv "$@" ~/.Trash ; }     # trash:        Moves a file to the MacOS trash
+ql () { qlmanage -p "$*" >& /dev/null; }    # ql:           Opens any file in MacOS Quicklook Preview
+alias finderShowHidden='defaults write com.apple.finder ShowAllFiles TRUE'
+alias finderHideHidden='defaults write com.apple.finder ShowAllFiles FALSE'
