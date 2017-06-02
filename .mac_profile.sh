3bb2f4fe2cb691be13b583a274cf3f8894caedc8
Jai Broome <jb@jaibroome.com>
2017-06-02 10:15:27 -0700

    unix-ized line endings

--- .mac_profile.sh
+++ .mac_profile.sh
@@ -1,11 +1,10 @@
 # Preferences specific to macOS machines
-
 # Aliases
-alias f='open -a Finder ./'                 # f:            Opens current directory in MacOS Finder
-trash () { command mv "$@" ~/.Trash ; }     # trash:        Moves a file to the MacOS trash
-ql () { qlmanage -p "$*" >& /dev/null; }    # ql:           Opens any file in MacOS Quicklook Preview
+alias f='open -a Finder ./'
+trash () { command mv "$@" ~/.Trash ; } 
+ql () { qlmanage -p "$*" >& /dev/null; } 
 alias finderShowHidden='defaults write com.apple.finder ShowAllFiles TRUE'
 alias finderHideHidden='defaults write com.apple.finder ShowAllFiles FALSE'
 
-PROFILE_ORDER=$PROFILE_ORDER:".mac_profile.sh"
+PROFILE_ORDER=$PROFILE_ORDER:'.mac_profile.sh'
 export PROFILE_ORDER
